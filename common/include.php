53716388e7fafc9e0bc0b5ef5aca584738ffd835
admire93 <khj983@lycos.co.kr>
2009-07-06 20:03:02 +0900

    include.php

--- common/include.php
+++ common/include.php
@@ -1,5 +1,13 @@
 <?php
 function __autoload($file) {
- require_once $file . '.php';
+	if(strpos($file,'Controller') !== false) {
+		require_once DIREC . '/controller/' . $file '.php';
+	} else if(strpos($file,'Model') {
+		require_once DIREC . '/model/' . $file . '.php';
+	} else if(strpos($file,'View') {
+		require_once DIREC . '/view/' . $file . '.php';
+	} else {
+		require_once DIREC . '/class/' . $file . '.class.php';
+	}
 }
 ?>
