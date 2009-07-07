0dacddb9306d5d72cae59cad3011e9b0dca4d88b
admire93 <khj983@lycos.co.kr>
2009-07-07 21:30:36 +0900

    .~

---
+++ controller/BoardController.php
@@ -0,0 +1,19 @@
+<?php
+class BoardController
+{
+	public function index()
+	{
+		echo '<h1>Welcome to the index page</h1>';
+	}
+	public function edit()
+	{
+	}
+	public function show()
+	{
+	}
+	public function lists()
+	{
+	}
+}
+?>
+
